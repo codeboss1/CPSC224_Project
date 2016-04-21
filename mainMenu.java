ba2322d4f1f554e3b6740e01381ba25c38973db2
kevindo1234 <kevindo0511@hotmail.com>
2016-04-21 11:09:46 -0700

    Update mainMenu.java

--- mainMenu.java
+++ mainMenu.java
@@ -1,3 +1,4 @@
+package gameHub;
 /*@author: Kevin Do
  *@version 1.0
  *@since   4/21/16
@@ -80,32 +81,32 @@ public abstract class mainMenu extends JFrame implements ActionListener{
 		//create button to change user
 		userbutton.setText("Change User");
 		userbutton.setBackground(new Color(131, 250, 20));
-		userbutton.setPreferredSize(new Dimension(200, 75));
+		userbutton.setPreferredSize(new Dimension(175, 50));
 		userbutton.setFont(new Font("Arial", Font.PLAIN, 20));
 
 				
 		//create button for tic tac toe
 		tictactoeButton.setText("Tic-Tac-Toe");
 		tictactoeButton.setBackground(new Color(131, 250, 20));
-		tictactoeButton.setPreferredSize(new Dimension(200, 75));
+		tictactoeButton.setPreferredSize(new Dimension(175, 50));
 		tictactoeButton.setFont(new Font("Arial", Font.PLAIN, 20));
 		
 		//create button for word search
 		wordSearchButton.setText("Word Search");
 		wordSearchButton.setBackground(new Color(131, 250, 20));
-		wordSearchButton.setPreferredSize(new Dimension(200, 75));
+		wordSearchButton.setPreferredSize(new Dimension(175, 50));
 		wordSearchButton.setFont(new Font("Arial", Font.PLAIN, 20));
 		
 		//create button for eye spy
 		ISpyButton.setText("Eye Spy");
 		ISpyButton.setBackground(new Color(131, 250, 20));
-		ISpyButton.setPreferredSize(new Dimension(200, 75));
+		ISpyButton.setPreferredSize(new Dimension(175, 50));
 		ISpyButton.setFont(new Font("Arial", Font.PLAIN, 20));
 		
 		//create button for high scores
 		highScoresButton.setText("High Scores");
 		highScoresButton.setBackground(new Color(131, 250, 20));
-		highScoresButton.setPreferredSize(new Dimension(200, 75));
+		highScoresButton.setPreferredSize(new Dimension(175, 50));
 		highScoresButton.setFont(new Font("Arial", Font.PLAIN, 20));
 	}
 	
@@ -127,8 +128,8 @@ public abstract class mainMenu extends JFrame implements ActionListener{
 		panel.setLayout(new FlowLayout());
 		panel.setBackground(new Color(186, 165, 232));frame.add(panel);
 		//set frame
-		frame.setSize(400, 550);
-		frame.setLocation(400,400);
+		frame.setSize(375, 450);
+		frame.setLocation(400,200);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
 
