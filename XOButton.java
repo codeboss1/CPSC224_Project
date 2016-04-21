abd93d00de44ec0a2a2c0c42f051b3260d7884d0
rcook2 <rcook2@zagmail.gonzaga.edu>
2016-04-20 17:13:06 -0700

    Added files via upload

---
+++ XOButton.java
@@ -0,0 +1,34 @@
+package gameHub;
+/*@author: Ryan Cook
+ *@version 1.0
+ *@since   4/19/16
+ *Class: CPSC 224-02
+ *Description: This is the button setup for tic-tac-toe
+ */
+
+
+import javax.swing.*;
+
+
+public class XOButton extends JButton {
+	ImageIcon X;
+	ImageIcon O;
+		
+	public XOButton() 
+	{
+		O = new ImageIcon("C:/jdk1.8.0_66/Homework/Group/src/Oimage.jpg");
+		X = new ImageIcon("C:/jdk1.8.0_66/Homework/Group/src/Ximage.jpg");
+		
+	}
+	
+	public void setPicture(char x)
+	{
+		if (x == 'x')
+			setIcon(X);
+		else if(x == 'o')
+			setIcon(O);
+		else
+			setIcon(null);
+	}
+}
+
